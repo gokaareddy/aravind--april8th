d53415d55763123ba0ce24d46f48acc9cfb01a9f
gokaareddy <100718143+gokaareddy@users.noreply.github.com>
2022-04-08 16:35:29 -0500

    Create bash shellscript2.sh
    duplicate of shellscript1

---
+++ bash shellscript2.sh
@@ -0,0 +1,7 @@
+echo "# aravind--april8th" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gokaareddy/aravind--april8th.git
+git push -u origin main
